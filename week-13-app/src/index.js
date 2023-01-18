d8b36d20137da1c7d8ae6beaebe3b2300c601a4d
dweisel <drewweisel@gmail.com>
2023-01-18 11:23:18 -0700

    updates

--- week-13-app/src/index.js
+++ week-13-app/src/index.js
@@ -8,7 +8,7 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-React.createElement(NewsFeed, {}, null),
+<NewsFeed />,
 );
 
 // If you want to start measuring performance in your app, pass a function
